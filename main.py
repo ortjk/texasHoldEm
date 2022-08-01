d29a657483defea60c77b0288a975298711351c2
LiamPower12 <liampower653@gmail.com>
2022-08-01 14:02:34 -0230

    Game window Complete

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import cards
 import calculations
-
 import random
-
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 # init deck
 deck = []
 for i in cards.suits:
@@ -10,4 +16,4 @@ for i in cards.suits:
         deck.append(cards.Card(i, q))
 
 random.shuffle(deck)
-
+print("hello world")
